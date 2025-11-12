d675c5d3701921d33879b77e025fc979d21f521e
Hossam <43291152+Hoss008@users.noreply.github.com>
2025-11-12 14:05:38 +0300

    doctor's component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,13 @@
 import ButtonExample from "../_themeUseExample/buttonExample";
 import PatientDash from "../Features/Patient/Pages/PatientDash";
+import DoctorDash from "../Features/Doctor/Pages/DoctorDash";
 
 // Import global routing here
 function App() {
   return (
     <>
-    <PatientDash/>
-      {/* <ButtonExample /> */}
+    {/* <PatientDash/> */}
+    <DoctorDash/>
     </>
   );
 }
